f15b42aca53dce83eca64f42f67ccb619358f142
vikas <yadav1vikas1995.gmail.com>
2018-09-30 20:43:35 +0530

    first commit

---
+++ innotrack/app/src/main/java/com/vikas/innotrack/MainActivity.java
@@ -0,0 +1,48 @@
+package com.vikas.innotrack;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onKeyDown (int keyCode,
+                              KeyEvent event) {
+
+        // This is the center button for headphones
+        if (event.getKeyCode() == KeyEvent.KEYCODE_HEADSETHOOK) {
+                final Thread th=new Thread()
+                {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }finally {
+                            Intent i=new Intent(MainActivity.this,OnActivity.class);
+                            startActivity(i);
+                        }
+                    }
+                };
+                th.start();
+
+                //Toast.makeText(this, "BUTTON on!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
